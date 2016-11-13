466c95f77e486331ee95ed1e884a39dc168f7532
MaartenGDev <maarten.grootoonk@gmail.com>
2016-11-13 17:49:10 +0100

    Updated counter

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -7,7 +7,7 @@
                 <h2 class="job-title">{{ $job->name }}</h2>
                 <section>
                     <p class="job-rating"><i class="fa fa-check job-pros" aria-hidden="true"></i> {{ count($job->pros) }} pluspunten</p>
-                    <p class="job-rating"><i class="fa fa-times job-cons" aria-hidden="true"></i> {{ count($job->tags) }} minpunten</p>
+                    <p class="job-rating"><i class="fa fa-times job-cons" aria-hidden="true"></i> {{ count($job->cons) }} minpunten</p>
 
                     <div class="job-tags">
                         @foreach($job->tags as $tag)
