7e011b6c216716bbea7f7243dcacb55cd96d985b
MaartenGDev <maarten.grootoonk@gmail.com>
2016-12-14 11:52:55 +0100

    Added auto high adjust

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -11,3 +11,20 @@ mobileNavigation.addEventListener("click", () => {
 
     navigationIsShown = !navigationIsShown;
 });
+
+
+window.onload = () => {
+    console.log('lets go');
+    const jobs = document.querySelectorAll('.job-list__item');
+    let height = 0;
+
+    jobs.forEach(job => {
+        if(job.offsetHeight > height){
+            height = job.offsetHeight;
+        }
+    });
+
+    jobs.forEach(job => {
+       job.style.height = (height + 50) + 'px';
+    });
+}
